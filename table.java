656f78f94803087f2d07388693ab5dfb3bbce052
yadav-amit11 <122402537+yadav-amit11@users.noreply.github.com>
2023-02-04 15:06:49 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(1 * 1 + "\t" + 2 * 1 + "\t" + 3 * 1 + "\t" + 4 * 1 + "\t" + 5 * 1);
+        System.out.println(1 * 2 + "\t" + 2 * 2 + "\t" + 2 * 3 + "\t" + 2 * 4 + "\t" + 2 * 5);
+        System.out.println(1 * 3 + "\t" + 2 * 3 + "\t" + 3 * 3 + "\t" + 3 * 4 + "\t" + 3 * 5);
+        System.out.println(1 * 4 + "\t" + 2 * 4 + "\t" + 4 * 3 + "\t" + 4 * 4 + "\t" + 4 * 5);
+    }
+}
